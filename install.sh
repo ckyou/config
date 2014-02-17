0c095a1117f5ed0f4d6d0cc110f44a3d5bf97e04
ckyou <aluksforgithub@gmail.com>
2014-02-18 00:30:23 +0800

    depend on vim version to put config; update vimrc

--- install.sh
+++ install.sh
@@ -11,3 +11,16 @@ cp ${BASEDIR}/.screenrc ~/
 
 # vim setting
 cp -R ${BASEDIR}/.vim ~/
+
+## since Vim 7.4, user can vimrc into $HOME/.vim/vimrc 
+## so, if vim version is less than 7.4, make a link 
+vim_version=`vim --version | grep "Vi IMproved" | awk '{print $5}'`
+result=`awk 'BEGIN{ print '$vim_version' < 7.4  }'`  
+
+if [ -L ~/.vimrc  ]; then  
+    rm -f ~/.vimrc
+fi
+
+if [ "$result" -eq 1 ];then
+    ln -s ~/.vim/vimrc ~/.vimrc
+fi
