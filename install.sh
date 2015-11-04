2682ae7ca05ca1dacca47fa8bbeab9966e81244d
ckyou <alwayskuso@gmail.com>
2015-11-04 23:03:33 +0800

    creating link instead of copy while run install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 
 # don't use .
-BASEDIR=`dirname $0`
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp ${BASEDIR}/.profile ~/
-cp ${BASEDIR}/.bashrc ~/
-cp ${BASEDIR}/.inputrc ~/
+
+ln -s ${BASEDIR}/.profile ~/.profile
+ln -s ${BASEDIR}/.bashrc ~/.bashrc
+ln -s ${BASEDIR}/.inputrc ~/.inputrc
 
 # screen setting
-cp ${BASEDIR}/.screenrc ~/
+ln -s ${BASEDIR}/.screenrc ~/.screenrc
 
 # tmux setting 
-cp ${BASEDIR}/.tmux.conf ~/
+ln -s ${BASEDIR}/.tmux.conf ~/.tmux.conf
 
 # vim setting
-cp -R ${BASEDIR}/.vim ~/
+ln -s  ${BASEDIR}/.vim ~/.vim 
 
 ## since Vim 7.4, user can vimrc into $HOME/.vim/vimrc 
 ## so, if vim version is less than 7.4, make a link 
-vim_version=`vim --version | grep "Vi IMproved" | awk '{print $5}'`
-result=`awk 'BEGIN{ print '$vim_version' < 7.4  }'`  
+#vim_version=`vim --version | grep "Vi IMproved" | awk '{print $5}'`
+#result=`awk 'BEGIN{ print '$vim_version' < 7.4  }'`  
 
 if [ -L ~/.vimrc  ]; then  
     rm -f ~/.vimrc
 fi
 
-if [ "$result" -eq 1 ];then
+#if [ "$result" -eq 1 ];then
     ln -s ~/.vim/vimrc ~/.vimrc
-fi
+#fi
